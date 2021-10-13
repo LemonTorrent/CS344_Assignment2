51faabd5d2bd1c65db97953bb24551e5e98fd8f6
LemonTorrent <nyarbrough14@gmail.com>
2021-10-13 12:49:37 -0700

    Messing around with opening file options

--- assignment2.c
+++ assignment2.c
@@ -5,42 +5,140 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdbool.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <time.h>
 
-int main(int argc, char *argv[]) {
+#define PREFIX "movies_"
+#define SUFFIX ".csv"
+
+int userInput(){
     int continueChoice = 0;
-    int choice = 0;
-    
-    do {
+
+    // do {
         printf("1. Select file to process\n");
         printf("2. Exit the program\n\n");
 
         printf("Enter a choice 1 or 2: ");
         scanf("%d", &continueChoice);
 
-        if (continueChoice != 2){
-            printf("\nWhich file do you want to process?\n");
-            printf("Enter 1 to pick the largest file\n");
-            printf("Enter 2 to pick the smallest file\n");
-            printf("Enter 3 to specify the name of a file\n\n");
 
-            printf("Enter a choice from 1 to 3: ");
-            scanf("%d", &choice);
-
-            if (choice == 1){
-                NULL;
-            } else if (choice == 2) {
-                NULL;
-            } else if (choice == 3){
-                NULL;
-            } else {
-                NULL;
-            }
-
-        }
         
         //printf("\n\n")
 
-    } while (continueChoice != 2);
+    // } while (continueChoice != 2);
+    if (continueChoice == 2){
+        continueChoice = 0;
+    }
+    return continueChoice;
+
+}
+
+int processFile() {
+    int choice = 0;
+
+    printf("\nWhich file do you want to process?\n");
+    printf("Enter 1 to pick the largest file\n");
+    printf("Enter 2 to pick the smallest file\n");
+    printf("Enter 3 to specify the name of a file\n\n");
+
+    printf("Enter a choice from 1 to 3: ");
+    scanf("%d", &choice);
+    return choice;
+}
+
+int largest(){
+    // Open the current directory
+    DIR* currDir = opendir(".");
+    struct dirent *aDir;
+    time_t lastModifTime;
+    struct stat dirStat;
+    int i = 0;
+    char entryName[256];
+
+    // Go through all the entries
+    while((aDir = readdir(currDir)) != NULL){
+
+        if(strncmp(PREFIX, aDir->d_name, strlen(PREFIX)) == 0){
+            // Get meta-data for the current entry
+            stat(aDir->d_name, &dirStat);  
+
+            // Use the difftime function to get the time difference between the current value of lastModifTime and the st_mtime value of the directory entry
+            if(i == 0 || difftime(dirStat.st_mtime, lastModifTime) > 0){
+                lastModifTime = dirStat.st_mtime;
+                memset(entryName, '\0', sizeof(entryName));
+                strcpy(entryName, aDir->d_name);
+            }
+            i++;
+        }
+        }
+    // Close the directory
+    closedir(currDir);
+    printf("The last file/directory starting with the prefix \"%s\" modified in the current directory is %s\n", PREFIX, entryName);
+    return 0;
+}
+
+int smallest(){
+    NULL;
+}
+
+int openName(){
+    DIR* currDir = opendir(".");
+    struct dirent *aDir;
+    time_t lastModifTime;
+    struct stat dirStat;
+    int i = 0;
+    char entryName[256];
+
+    char str[] = "movies_sample_1.csv";
+
+    // Go through all the entries
+    while((aDir = readdir(currDir)) != NULL){
+
+        if(strncmp(str, aDir->d_name, strlen(str)) == 0){
+            // Get meta-data for the current entry
+            stat(aDir->d_name, &dirStat);  
+
+            // Use the difftime function to get the time difference between the current value of lastModifTime and the st_mtime value of the directory entry
+            if(i == 0 || difftime(dirStat.st_mtime, lastModifTime) > 0){
+                lastModifTime = dirStat.st_mtime;
+                memset(entryName, '\0', sizeof(entryName));
+                strcpy(entryName, aDir->d_name);
+            }
+            i++;
+        }
+    }
+    // Close the directory
+    closedir(currDir);
+    printf("The last file/directory starting with the prefix \"%s\" modified in the current directory is %s\n", str, entryName);
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    int choice = 0;
+
+    largest();
+    // openName();
+
+    return;
+
+    do {
+        choice = userInput();
+
+        if (choice != 0){
+            choice = processFile();
+        }
+
+        if (choice == 1){
+            largest();
+        } else if (choice == 2){
+            smallest();
+        } else if (choice == 3){
+            openName();
+        }
+
+    } while (choice != 0);
+    
 
 	return 0;
 }
